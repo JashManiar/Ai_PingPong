07e2969db3f92fb02c808f6303f7714874c4415e
Jash Maniar <101001262+JashManiar@users.noreply.github.com>
2022-12-13 18:03:19 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -22,8 +22,12 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(1000,450);
-  canvas.center();
+  var canvas = createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
 }
 
 
